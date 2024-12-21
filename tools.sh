0d949052ff7da92024f59ec7b583a52095cc3395
Muller Matos <98285191+millerjmatos@users.noreply.github.com>
2024-12-21 09:41:42 -0300

    Create tools.sh

---
+++ tools.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+sudo apt update && sudo apt upgrade -y
+
+sudo apt install git -y
+
+# Configura o Git (substitua pelos seus dados):
+git config --global user.name "Muller Matos"
+git config --global user.email "millerjmatos@hotmail.com"
+
+git clone https://github.com/millerjmatos/bootstrap.git ~/ansible-setup
+
+sudo apt install ansible -y
+
+# Executa o playbook:
+cd ~/ansible-setup
+ansible-playbook tools.yml --ask-become-pass
